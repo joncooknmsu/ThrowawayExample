a92aedadf20a4827c93d9826c8ca9b11b204156f
Jonathan Cook <joncook@nmsu.edu>
2023-09-28 11:40:33 -0600

    initial interactor code

---
+++ Interactor.java
@@ -0,0 +1,24 @@
+
+public class Interactor
+{
+
+public Interactor()
+{
+    System.out.println("Welcome to the Interactor!");
+}
+
+public void interact(String prompt)
+{
+    System.out.println("I should respond to (" + prompt + ")");
+}
+
+public static void main(String[] args)
+{
+    Interactor bot = new Interactor();
+    for (String arg: args) {
+        bot.interact(arg);
+    }
+}
+
+} // end class
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ThrowawayExample
+
+This repository is used for doing simple code examples, and
+for showing repository and GitHub operations.
