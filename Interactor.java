057d1ee5054b0b1c9f5ea16f46a81bb19c4f8dba
Jonathan Cook <joncook@nmsu.edu>
2024-09-26 14:36:38 -0600

    fixed syntax error

--- Interactor.java
+++ Interactor.java
@@ -12,7 +12,7 @@ public void interact(String prompt)
     System.out.println("I should respond to (" + prompt + ")");
     if (prompt.equals("what")) {
         System.out.println("What does the fox say?");
-    if (prompt.equals("who")) {
+    } else if (prompt.equals("who")) {
         System.out.println("Who ya gonna call? Ghostbusters!");
     }
 }
